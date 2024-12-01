c604f5fe42580241a51816baed41a7c0fb140612
Ricardo Estrella <restrella@nasrecruitment.com>
2024-11-30 23:49:29 -0500

    Update main height

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 import React from "react";
 import Hero from "./components/Hero";
 import "../app/globals.css";
@@ -15,12 +15,12 @@ const Home: React.FC = () => {
       </Head>
 
       <div className="min-h-full grid grid-rows-[auto,1fr,auto]">
-        <main className="bg-background text-primary row-span-4 min-h-full">
+        <main className="bg-background text-primary row-span-4 min-h-full lg:min-h-[91.15vh]">
           <Hero />
           <section className="text-center py-8 flex flex-col lg:flex-row md: justify-center lg:justify-evenly items-center">
             <div className="flex flex-col">
               <h2 className="text-2xl md:text-3xl font-bold mb-4">Cupcakes</h2>
-              <Slideshow items={cupcakes} time={2500}/>
+              <Slideshow items={cupcakes} time={2500} />
             </div>
             <div className="flex flex-col">
               <h2 className="text-2xl md:text-3xl font-bold mt-8 mb-4">
